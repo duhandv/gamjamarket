704446701a9e2e86fafff2bf879d79b86413c010
Duhan Mo <devduhanmo@gmail.com>
2024-02-15 23:36:52 +0900

    first-commit

---
+++ src/main/java/project/duhan/gamjamarket/GamjamarketApplication.java
@@ -0,0 +1,13 @@
+package project.duhan.gamjamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjamarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamjamarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/duhan/gamjamarket/GamjamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package project.duhan.gamjamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjamarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
