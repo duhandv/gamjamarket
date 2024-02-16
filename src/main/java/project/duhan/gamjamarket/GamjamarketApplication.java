eadabd9d06bdce40df37d0f4e33b95e4006ac6c6
duhandv <duhandv@gmail.com>
2024-02-16 18:53:54 +0900

    Apply fixture monkey

--- src/main/java/project/duhan/gamjamarket/GamjamarketApplication.java
+++ src/main/java/project/duhan/gamjamarket/GamjamarketApplication.java
@@ -1,13 +1,21 @@
 package project.duhan.gamjamarket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GamjamarketApplication {
 
+    public static final Logger logger = LoggerFactory.getLogger(GamjamarketApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(GamjamarketApplication.class, args);
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
     }
 
 }
