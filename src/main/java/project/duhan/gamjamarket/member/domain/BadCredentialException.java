dc9491845a0c6e3e09a0fb1e65b1a41aa969b861
Duhan Mo <duhandv@gmail.com>
2024-02-21 17:35:07 +0900

    Apply jacoco

--- src/main/java/project/duhan/gamjamarket/member/domain/BadCredentialException.java
+++ src/main/java/project/duhan/gamjamarket/member/domain/BadCredentialException.java
@@ -5,21 +5,4 @@ public class BadCredentialException extends RuntimeException {
     public BadCredentialException() {
     }
 
-    public BadCredentialException(String message) {
-        super(message);
-    }
-
-    public BadCredentialException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public BadCredentialException(Throwable cause) {
-        super(cause);
-    }
-
-    public BadCredentialException(String message, Throwable cause, boolean enableSuppression,
-            boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 }
